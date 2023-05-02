65200983facf6cc11a38b334845aae181d44b6ae
vmcabreu <victormanueltoa@gmail.com>
2023-05-02 03:00:48 +0100

    Config back

--- km/backend/controller/bebida/list.php
+++ km/backend/controller/bebida/list.php
@@ -7,16 +7,23 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
         if (strpos($categoria,'_') !== false) {
             $categoria = str_replace("_"," ",$categoria);
             $listaBebidas = DAOBebida::listaBebidaPorCategoria($categoria);
+            if ($listaBebidas != null) {
+                echo json_encode($listaBebidas,JSON_UNESCAPED_UNICODE);
+            } else {
+                http_response_code(404);
+                echo json_encode(array("message" => "No se encontró el usuario con ID " . $id));
+            }
         }else{
             $listaBebidas = DAOBebida::listaBebidaPorCategoria($categoria);
+            if ($listaBebidas != null) {
+                echo json_encode($listaBebidas,JSON_UNESCAPED_UNICODE);
+            } else {
+                http_response_code(404);
+                echo json_encode(array("message" => "No se encontró el usuario con ID " . $id));
+            }
         }
 
-        if ($listaBebidas != null) {
-            echo json_encode($listaBebidas,JSON_UNESCAPED_UNICODE);
-        } else {
-            http_response_code(404);
-            echo json_encode(array("message" => "No se encontró el usuario con ID " . $id));
-        }
+        
     }else{
         $listaBebidas = DAOBebida::listaBebida();
         if ($listaBebidas != null) {
