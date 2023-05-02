60946bf985563d72eb6f70f7095ea3fd5843e2bb
vmcabreu <victormanueltoa@gmail.com>
2023-05-02 03:08:53 +0100

    Config back

--- km/backend/controller/comida/list.php
+++ km/backend/controller/comida/list.php
@@ -3,12 +3,13 @@ require_once(__DIR__ . "../../../inc/bootstrap.php");
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     if (isset($_GET['categoria'])) {
         $categoria = $_GET['categoria'];
+        $categoria = str_replace('_', ' ', $categoria);
         $listaComida = DAOComida::listaComidaPorCategoria($categoria);
         if ($listaComida != null) {
             echo json_encode($listaComida,JSON_UNESCAPED_UNICODE);
         } else {
-            http_response_code(404);
-            echo json_encode(array("message" => "No se encontró el usuario con ID " . $id));
+            http_response_code(404);            
+            echo json_encode(array("message" => "No se encontró la comida con la categoría  " . $categoria));
         }
     }else{
         $listaComida = DAOComida::listaComida();
