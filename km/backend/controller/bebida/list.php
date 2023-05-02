6380f3785c24eae6ec02cf633d69fd1d0993687a
vmcabreu <victormanueltoa@gmail.com>
2023-05-02 03:07:45 +0100

    Config back

--- km/backend/controller/bebida/list.php
+++ km/backend/controller/bebida/list.php
@@ -4,18 +4,18 @@ require_once(__DIR__ . "../../../inc/bootstrap.php");
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     if (isset($_GET['categoria'])) {
         $categoria = $_GET['categoria'];
-        $categoria = str_replace('_', ' ',$categoria);
+        $categoria = str_replace('_', ' ', $categoria);
         $listaBebidas = DAOBebida::listaBebidaPorCategoria($categoria);
         if ($listaBebidas != null) {
-            echo json_encode($listaBebidas,JSON_UNESCAPED_UNICODE);
+            echo json_encode($listaBebidas, JSON_UNESCAPED_UNICODE);
         } else {
             http_response_code(404);
-            echo json_encode(array("message" => "No se encontró el usuario con ID " . $id));
+            echo json_encode(array("message" => "No se encontró la comida con la categoría  " . $categoria));
         }
-    }else{
+    } else {
         $listaBebidas = DAOBebida::listaBebida();
         if ($listaBebidas != null) {
-            echo json_encode($listaBebidas,JSON_UNESCAPED_UNICODE);
+            echo json_encode($listaBebidas, JSON_UNESCAPED_UNICODE);
         } else {
             http_response_code(404);
             echo json_encode(array("message" => "No se encontró el usuario con ID " . $id));
