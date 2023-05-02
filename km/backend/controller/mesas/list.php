fd0a1cb90680ed68ad14aebc8642ca310e1c3ace
vmcabreu <victormanueltoa@gmail.com>
2023-05-02 05:10:21 +0100

    Mesas backend

--- km/backend/controller/mesas/list.php
+++ km/backend/controller/mesas/list.php
@@ -1,23 +1,11 @@
 <?php
 require_once(__DIR__ . "../../../inc/bootstrap.php");
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-    if (isset($_GET['categoria'])) {
-        $categoria = $_GET['categoria'];
-        $categoria = str_replace('_', ' ', $categoria);
-        $listaMesas = DAOMesa::listaMesas();
-        if ($listaComida != null) {
-            echo json_encode($listaMesas,JSON_UNESCAPED_UNICODE);
-        } else {
-            http_response_code(404);            
-            echo json_encode(array("message" => "No se encontró la comida con la categoría  " . $categoria));
-        }
-    }else{
-        $listaComida = DAOComida::listaComida();
-        if ($listaComida != null) {
-            echo json_encode($listaComida,JSON_UNESCAPED_UNICODE);
-        } else {
-            http_response_code(404);
-            echo json_encode(array("message" => "No se encontró el usuario con ID " . $id));
-        }
+    $listaMesas = DAOMesa::listaMesas();
+    if ($listaComida != null) {
+        echo json_encode($listaMesas,JSON_UNESCAPED_UNICODE);
+    } else {
+        http_response_code(404);            
+        echo json_encode(array("message" => "No se encontró la lista de mesas"));
     }
 }
