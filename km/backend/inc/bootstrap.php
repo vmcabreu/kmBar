a63d45693bfa72fdef079558ac91727bb88f2c0e
vmcabreu <victormanueltoa@gmail.com>
2023-05-02 01:21:25 +0100

    Configuracion back

--- km/backend/inc/bootstrap.php
+++ km/backend/inc/bootstrap.php
@@ -5,3 +5,5 @@ require_once PROJECT_ROOT_PATH . "/inc/cors.php";
 require_once PROJECT_ROOT_PATH . "/model/BaseDAO.php";
 require_once PROJECT_ROOT_PATH . "/model/Bebida.php";
 require_once PROJECT_ROOT_PATH . "/model/Comida.php";
+require_once PROJECT_ROOT_PATH . "/model/DAOBebida.php";
+require_once PROJECT_ROOT_PATH . "/model/DAOComida.php";
