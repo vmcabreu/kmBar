54413773d8d941998fda612dc179989926bd1aad
vmcabreu <victormanueltoa@gmail.com>
2023-06-17 03:52:16 +0100

    Fixing errores y modificando apariencia

--- km/frontend/src/app/components/ingresos/ingresos.component.ts
+++ km/frontend/src/app/components/ingresos/ingresos.component.ts
@@ -35,30 +35,35 @@ export class IngresosComponent {
       next: (comandas: any[]) => {
         let total = 0;
         let fechaActual = this.getFechaActual();
-
         comandas.forEach(element => {
           if (element.fecha == fechaActual) {
             total += element.total
           }
         });
-        this.ingresosService.terminarJornada(total).subscribe({
-          next:()=>{
-            this.getIngresos()
-          },
-          error:()=>{
-            this.ingresosService.updateJornada(total).subscribe({
-              next:()=>{
-                this.getIngresos()
-              },
-              error:(err:any)=>{
-                console.log("");
+        if (!this.checkFecha) {
+          this.ingresosService.terminarJornada(total).subscribe({
+            next: () => {
+              this.getIngresos()
+            },
+            error: (err: any) => {
+              console.log(err);
 
-              }
-            })
-          }
-        })
+            }
+          })
+        } else {
+          this.ingresosService.updateJornada(total).subscribe({
+            next: () => {
+              this.getIngresos()
+            },
+            error: (err: any) => {
+              console.log(err);
+
+            }
+          })
+        }
       },
-      error:()=>{
+      error: (err: any) => {
+        console.log(err);
 
       }
     })
@@ -67,13 +72,16 @@ export class IngresosComponent {
   getFechaActual() {
     const fechaActual = new Date();
     const anio = fechaActual.getFullYear();
-    const mes = fechaActual.getMonth() + 1; // Los meses comienzan desde 0, por lo que sumamos 1
+    const mes = fechaActual.getMonth() + 1;
     const dia = fechaActual.getDate();
-
-    // Aseguramos que los valores de mes y día tengan dos dígitos
     const mesStr = mes < 10 ? `0${mes}` : `${mes}`;
     const diaStr = dia < 10 ? `0${dia}` : `${dia}`;
-
     return `${anio}-${mesStr}-${diaStr}`;
   }
+
+  checkFecha(): any {
+    let fechaActual = this.getFechaActual();
+    return this.listaIngresos.find(ingreso => ingreso.fecha === fechaActual);
+  }
+
 }
